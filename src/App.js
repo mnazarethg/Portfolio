3a7bcd7da6d28674ea70ae15080325ded07c95e1
mnazarethg <mnazarethg@gmail.com>
2024-11-12 14:13:43 +0100

    router basename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-    <Router>
+    <Router basename="/portfolio">
         <Header />
         <Routes>
         <Route path="/portfolio" element={<PrincipalPage />} />
